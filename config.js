bfa415a967ac065a2982ae8a175df2db2aaa9547
Servicios Generales <serviciosgeneralesaesa1990@gmail.com>
2026-09-05 10:18:00 -0500

    Add files via upload

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 const FIREBASE_CONFIG = {
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_PROYECTO.firebaseapp.com",
-  databaseURL: "https://TU_PROYECTO-default-rtdb.firebaseio.com",
-  projectId: "TU_PROYECTO",
-  storageBucket: "TU_PROYECTO.appspot.com",
-  messagingSenderId: "TU_MESSAGING_SENDER_ID",
-  appId: "TU_APP_ID"
+  apiKey: "AIzaSyB-EjemploDeTuLlaveLarga...",
+  authDomain: "comedores-mina.firebaseapp.com",
+  databaseURL: "https://comedores-mina-default-rtdb.firebaseio.com",
+  projectId: "comedores-mina",
+  storageBucket: "comedores-mina.appspot.com",
+  messagingSenderId: "1234567890",
+  appId: "1:1234567890:web:abcde12345"
 };
